c6608bccd84ad063c9879254b4845c3bfa659120
Davi Campos <arlocm@gmail.com>
2012-11-30 15:23:46 -0200

    Creating defined type to create a watcher in circusd

---
+++ manifests/watcher.pp
@@ -0,0 +1,54 @@
+define circus::watcher(
+  $ensure = present,
+  $cmd,
+  $args = undef,
+  $shell = undef,
+  $working_dir = undef,
+  $uid = undef,
+  $gid = undef,
+  $copy_env = undef,
+  $copy_path = undef,
+  $warmup_delay,
+  $numprocesses,
+  $relimit = undef,
+  $stderr_stream_class = undef,
+  $stderr_stream_opts = undef,
+  $stdout_stream_class = undef,
+  $stdout_stream_opts = undef,
+  $send_hup = undef,
+  $max_retry = undef,
+  $priority = undef,
+  $singleton = undef,
+  $use_sockets = undef,
+  $max_age = undef,
+  $max_age_variance = undef,
+  $hooks = undef,
+  $extra_conf = {}
+) {
+
+  file { "circus-watcher-${name}":
+    ensure  => $ensure,
+    path    => "${circus::include_dir}/watcher-${name}.ini",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('circus/watcher.ini.erb'),
+    notify  => Service['circus'],
+  }
+
+  if $ensure == present {
+    service { "circus-watcher-${name}":
+      ensure     => running,
+      path       =>  ['/usr/bin'],
+      start      => "circusctl start ${name}",
+      restart    => "circusctl restart ${name}",
+      stop       => "circusctl stop ${name}",
+      status     => "circusctl status ${name} | grep active",
+      hasrestart => false, 
+      hasstatus  => false,
+      provider   => base,
+      subscribe  => File["circus-watcher-${name}"],
+    }
+  }
+
+}
